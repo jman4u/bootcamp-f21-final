de6de265806cb4fd756c00f64db4c0012c1af61c
jman4u <67943741+jman4u@users.noreply.github.com>
2021-11-11 19:55:04 -0500

    initial configs

---
+++ src/pages/api/posts.js
@@ -0,0 +1,12 @@
+
+/*
+import mongodb from "../../../server/mongodb"
+
+export default async function handler(req, res) {
+  const db = await mongodb();
+  const restaurants = db.collection('restaurants');
+  //const results = await restaurants.findOne()
+  //console.log(results)
+  res.status(200).json(restaurants);
+}
+*/
